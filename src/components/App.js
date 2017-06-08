050550fe300623ecbad92443972bbe22867ca860
kavunshiva <kavunshiva@gmail.com>
2017-06-08 15:28:37 -0400

    fake news API complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
 
   constructor() {
     super();
-    
+
     this.state = {
       latestTweets: []
     };
@@ -28,6 +28,18 @@ class App extends React.Component {
     this.setState({
       latestTweets: newTweets
     });
+  }
+
+  componentWillMount(){
+    this.fetchTweets()
+  }
+
+  componentDidMount(){
+    this.startInterval()
+  }
+
+  componentWillUnmount(){
+    this.cleanUpInterval()
   }
 
   render() {
